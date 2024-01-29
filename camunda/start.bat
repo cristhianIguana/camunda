2629a8f1cca7738d4c18dc96843440fb56585b88
Administrador <root>
2024-01-29 08:17:49 -0500

    camund

---
+++ camunda/start.bat
@@ -0,0 +1,24 @@
+@echo off
+
+SET BASEDIR=%~dp0
+SET EXECUTABLE=%BASEDIR%internal\run.bat
+
+REM Start detached if no arguments
+IF [%~1]==[] GOTO StartDetached
+
+REM Collect arguments to pass to the executable
+SET CMD_LINE_ARGS=
+:setArgs
+IF [%~1]==[] GOTO StartWithArguments
+SET CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+SHIFT
+GOTO setArgs
+
+:StartDetached
+call "%EXECUTABLE%" start --detached
+GOTO Done
+
+:StartWithArguments
+call "%EXECUTABLE%" start %CMD_LINE_ARGS%
+
+:Done
